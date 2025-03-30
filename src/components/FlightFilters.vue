390debcd971cf79434230b519ded64f62e2b77cc
elprii <elprii@taltech.ee>
2025-03-31 02:28:44 +0300

    update flight filters

--- src/components/FlightFilters.vue
+++ src/components/FlightFilters.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <label>Origin: <input v-model="filters.origin" /></label>
+    <span v-if="error" style="color: red;">{{ error }}</span>
+
     <label>Destination: <input v-model="filters.destination" /></label>
     <label>Date: <input type="date" v-model="filters.date" /></label>
     <label>Max Price: <input type="number" v-model="filters.maxPrice" /></label>
     <label>Min Duration: <input type="number" v-model="filters.minDuration" /></label>
+
     <button @click="applyFilters">Apply Filters</button>
   </div>
 </template>
@@ -13,17 +17,25 @@ export default {
   data() {
     return {
       filters: {
+        origin: '',
         destination: '',
         date: '',
         maxPrice: '',
         minDuration: ''
-      }
+      },
+      error: ''
     };
   },
   methods: {
     applyFilters() {
+      if (!this.filters.origin.trim()) {
+        this.error = "Origin is required!";
+        return;
+      }
+      this.error = '';
       this.$emit('applyFilters', this.filters);
     }
   }
 };
 </script>
+
